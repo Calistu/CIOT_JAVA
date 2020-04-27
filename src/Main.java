aa73039589c3b2b5ac67d7267d58222fe2886be5
Calistu <jovictor210@gmail.com>
2020-04-27 01:37:37 -0300

    caminho xml por argv[]

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
 	public static void main(String[] args) {	
-		/*if(args.length<=0)
+		if(args.length<=0)
 		{
 			System.out.println("Digite o caminho do arquivo XML");
 			return ;
-		}*/
+		}
 		Operacao xml = new Operacao();
 		NovaOperacao leitura = new NovaOperacao();
 		CriarJSON escrita = new CriarJSON();  
